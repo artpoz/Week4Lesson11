c78c150bfe13ac5d4e99604298923054cc2e06c6
Artur Poznański <apoznan5@gmail.com>
2025-03-31 14:49:40 +0200

    odd and even

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace Week4Lesson11
     {
         static void Main(string[] args)
         {
+            var number = GetNumber();
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Podałeś liczbę parzystą");
+            }
+            else
+                Console.WriteLine("Podałeś liczbę nieparzystą");
+        }
+
+        public static int GetNumber()
+        {
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę:");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane. Spróbuj ponownie.");
+                    continue;
+                }
+                return number;
+            }            
         }
     }
 }
